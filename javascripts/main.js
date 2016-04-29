d24516405fb959a3b16c3c39b7b77c629ce3c38e
Cherry Wang <v-cherrw@microsoft.com>
2016-04-28 21:55:45 -0700

    added some images for portfolio; fixed javascript closure and global variables

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,6 @@
+---
+---
+
 //RequireJS config
 require.config({
     paths: {
@@ -18,44 +21,71 @@ require.config({
     }
 });
 
-//create charts
-require(['jquery', 'Chart'], function($, Chart) {
-    Chart.defaults.global.legend.display = false;
-    Chart.defaults.global.defaultFontColor = 'rgba(156,63,74, 1)';
 
-    var chartRadarDOM = $('#chartRadar');
-    var chartRadarData = {
-        labels: ["HTML / CSS", "JavaScript", "Database", "C#.NET / OOP / Algorithm", "Photoshop / Illustrator", "Git"],
-        datasets: [{
-            label: "Skill Level",
-            backgroundColor: "rgba(156,63,74,.5)",
-            borderColor: "rgba(156,63,74,.8)",
-            pointBackgroundColor: "rgba(156,63,74,1)",
-            pointBorderColor: "#fff",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(156,63,74,1)",
-            pointBorderWidth: 2,
-            data: [9, 7, 6, 5, 10, 9]
-        }]
+define(['jquery', 'Chart'], function($, Chart) {
+    //load global variables from _data/variables.json
+    var gloalVariables = {{ site.data.variables | jsonify }};
+    console.log(gloalVariables);
+
+    //convert hex string to rgba string
+    var hexToRgb = function(hex, opacity) { //hex: '#AAABBB'
+        hex = hex.replace('#', '');
+        r = parseInt(hex.substring(0, 2), 16);
+        g = parseInt(hex.substring(2, 4), 16);
+        b = parseInt(hex.substring(4, 6), 16);
+        return (opacity !== null && opacity !== undefined) ? 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')' : 'rgba(' + r + ',' + g + ',' + b + ',' + 1 + ')';
     };
-    var chartRadarOptions = {
-        scale: {
-            ticks: {
-                beginAtZero: true,
-                maxTicksLimit: 6
-            },
-            pointLabels: {
-                fontSize: 12
-            },
-            gridLines: {
-                color: 'rgba(156,63,74,.1)'
+
+    //convert rgba string to hex string
+    var rgbaToHex = function(rgba) { //rgba: 'rgba(255, 100, 155, 20)'
+        rgba = rgba.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+        return (rgba && rgba.length === 4) ? "#" +
+            ("0" + parseInt(rgba[1], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgba[2], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgba[3], 10).toString(16)).slice(-2) : '';
+    };
+
+    //create charts - I only have one chart for now. will make an object if more charts are added
+    var createCharts = function() {
+        Chart.defaults.global.legend.display = false;
+        Chart.defaults.global.defaultFontColor = hexToRgb(gloalVariables.colors['color-brand']);
+
+        var chartRadarDOM = $('#chartRadar');
+        var chartRadarData = {
+            labels: ["HTML / CSS", "JavaScript", "Database", "C#.NET / OOP / Algorithm", "Photoshop / Illustrator", "Git"],
+            datasets: [{
+                label: "Skill Level",
+                backgroundColor: hexToRgb(gloalVariables.colors['color-brand'], 50),
+                borderColor: hexToRgb(gloalVariables.colors['color-brand'], 80),
+                pointBackgroundColor: hexToRgb(gloalVariables.colors['color-brand']),
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: hexToRgb(gloalVariables.colors['color-brand']),
+                pointBorderWidth: 2,
+                data: [9, 7, 6, 5, 10, 9]
+            }]
+        };
+        var chartRadarOptions = {
+            scale: {
+                ticks: {
+                    beginAtZero: true,
+                    maxTicksLimit: 6
+                },
+                pointLabels: {
+                    fontSize: 12
+                },
+                gridLines: {
+                    color: hexToRgb(gloalVariables.colors['color-brand'], 10)
+                }
             }
-        }
-
+        };
+        var chartRadar = new Chart(chartRadarDOM, {
+            type: 'radar',
+            data: chartRadarData,
+            options: chartRadarOptions
+        });
     };
-    var chartRadar = new Chart(chartRadarDOM, {
-        type: 'radar',
-        data: chartRadarData,
-        options: chartRadarOptions
-    });
+
+    //call things
+    createCharts();
 });
